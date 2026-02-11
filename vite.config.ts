98a7cc6bb8a23bd65e9340363c710e28235a2009
Felipe Moreira <felipe.moreira.da.s@gmail.com>
2026-02-11 17:48:44 -0300

    Ajustes de responsividade

--- vite.config.ts
+++ vite.config.ts
@@ -10,15 +10,6 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        rollupOptions: {
-          output: {
-            format: 'es'
-          }
-        }
-      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
